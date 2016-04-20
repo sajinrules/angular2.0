8c86d3e8a70e281d219d53a34576273f2c4d9891
sajinrules <sajinrules@gmail.com>
2016-04-20 21:06:41 +0530

    registration

--- app/components/register.component.ts
+++ app/components/register.component.ts
@@ -1,6 +1,8 @@
 import {Component,View} from 'angular2/core';
+import { Router, RouterLink } from 'angular2/router';
 import {NgForm}    from 'angular2/common';
 import {User}    from '../model/user';
+import {authService} from '../services/auth.service';
 @Component({
 	selector: 'register-page'
 })
@@ -9,6 +11,15 @@ import {User}    from '../model/user';
 })
 export class RegisterPageComponent {
 	//public hero: Hero;
+	constructor(public router: Router,private _authService: authService) {
+	}
 	submitted = false;
 	user = new User();
+	onSubmit(){ 
+		if(this.user.email==="sajinrules@gmail.com" && this.user.password==="sajin"){
+			this.submitted = true; 
+			this._authService.setStatus(true);
+			this.router.parent.navigateByUrl('/home');
+		}
+	}
 }
